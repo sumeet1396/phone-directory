24ac50f0085162808fc75d0fe029244105d64375
rsumeetprdxn <sumeetr@prdxn.com>
2021-05-08 19:35:13 +0530

    base structure added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+        <header>
+          <h1>Phone Directory</h1>
+        </header>
+        <div>
+          <button>Add</button>
+          <div>
+            <span>Name</span><br />
+            <span>Phone</span>
+          </div>
+        </div>
     </div>
   );
 }
