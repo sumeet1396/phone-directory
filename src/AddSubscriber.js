c8b2b63ec8c65b2fdf90a628e2bd8c0823a091fa
rsumeetprdxn <sumeetr@prdxn.com>
2021-05-11 00:22:39 +0530

    onchange event added

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -4,6 +4,24 @@ import Header from './Component/Header';
 import './App.css';
 
 class AddSusbscriber extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            id: 0,
+            name: '',
+            phone: ''
+        }
+    }
+
+    inputChangedHandler = (event) => {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        });
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div>
@@ -13,14 +31,14 @@ class AddSusbscriber extends Component {
 
                     <form className="subscriber-form">
                         <label htmlFor="name" className="label-control">Name: </label><br/>
-                        <input id="name" type="text" className="input-control" name="name"/><br/><br/>
+                        <input id="name" type="text" className="input-control" name="name" onChange={this.inputChangedHandler} /><br/><br/>
                         <label htmlFor="phone" className="label-control">Phone: </label><br/>
-                        <input id="phone" type="text" className="input-control" name="phone"/><br/><br/>
+                        <input id="phone" type="text" className="input-control" name="phone" onChange={this.inputChangedHandler} /><br/><br/>
 
                         <div className="subscriber-info-container">
                             <span className="subscriber-to-add-heading">Subscriber to be added: </span><br/>
-                            <span className="subscriber-info">Name: </span><br/>
-                            <span className="subscriber-info">Phone: </span><br/>
+                            <span className="subscriber-info">Name: {this.state.name}</span><br/>
+                            <span className="subscriber-info">Phone: {this.state.phone}</span><br/>
                         </div>
 
                         <button type="submit" className="custom-btn add-btn">Add</button>
